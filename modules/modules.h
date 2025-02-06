725849a4d1f0c31746a7c821db724c2d53244fca
Aina Rajaonah Andriamanalimanda <aandriam@c1r4s9.42antananarivo.mg>
2025-02-06 18:01:06 +0300

    try to add rawmap

--- modules/modules.h
+++ modules/modules.h
@@ -6,7 +6,7 @@
 /*   By: aandriam <aandriam@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 15:04:32 by aandriam          #+#    #+#             */
-/*   Updated: 2025/02/06 16:32:36 by aandriam         ###   ########.fr       */
+/*   Updated: 2025/02/06 18:00:13 by aandriam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,40 @@
 
 typedef struct s_data
 {
-	char	*no;
-	char	*so;
-	char	*we;
-	char	*ea;
-	char	*f;
-	char	*c;
-}			t_data;
+	char						*no;
+	char						*so;
+	char						*we;
+	char						*ea;
+	char						*f;
+	char						*c;
+}								t_data;
 
 typedef struct s_flag
 {
-	int		map_init;
-	int		data_ok;
-}			t_flag;
+	int							map_init;
+	int							data_ok;
+}								t_flag;
+
+typedef struct s_raw_map
+{
+	char						*str;
+	int							str_len;
+	typedef struct s_raw_map	*next;
+}								t_raw_map;
 
 typedef struct s_vars
 {
-	int		argc;
-	char	**argv;
-	t_data	data;
-	char	**map;
-	t_flag	flags;
-}			t_vars;
+	int							argc;
+	char						**argv;
+	t_data						data;
+	char						**map;
+	t_flag						flags;
+	t_raw_map					*raw_map;
+}								t_vars;
 
-void		ft_perror(char *str, char *tmp, int exit_code, t_vars *vars);
-void		clean_vars(t_vars *vars);
-void		ptrptr_free(char ***lol);
+void							ft_perror(char *str, char *tmp, int exit_code,
+									t_vars *vars);
+void							clean_vars(t_vars *vars);
+void							ptrptr_free(char ***lol);
 
 #endif
